945cf64916af6a239adfaeab9ca3ff14b91de9b3
ruairi keane <ruairi.rk@icloud.com>
2024-01-25 10:51:09 +0000

    Set global variables

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = ["_" , "_" , "_"
+         "_" , "_" , "_"
+         "_" , "_" , "_"]
+player1 = "X"
+winner = None
+gameRunning = True
