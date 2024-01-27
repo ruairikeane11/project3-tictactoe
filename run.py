b9c381520bbcb6c7c102a0c8457826b704f2cdc8
ruairi keane <ruairi.rk@icloud.com>
2024-01-27 14:32:03 +0000

    Change design of board

--- run.py
+++ run.py
@@ -1,23 +1,23 @@
 import random
 
-board = ["--", "--", "--",
-         "--", "--", "--",
-         "--", "--", "--"]
+board = [" ", " ", " ",
+         " ", " ", " ",
+         " ", " ", " "]
 player1 = "X"
 winner = None
 gameRunning = True
 
 def boardGame(board):
     print(board[0] +     "|"     + board[1] +     "|"     + board[2])
-    print('------')
+    print('-----')
     print(board[3] +     "|"     + board[4] +     "|"     + board[5])
-    print('------')
+    print('-----')
     print(board[6] +     "|"     + board[7] +     "|"     + board[8])
 
 
 def playerInput(board):
     inp = int(input("Enter a number between 1-9: "))
-    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+    if inp >= 1 and inp <= 9 and board[inp-1] == " ":
         board[inp-1] = player1
     else:
         print('Sorry, this place has been taken!')
@@ -27,13 +27,13 @@ def checkAcross(board):
     Checks for horizonral rows of X and makes a winner
     """
     global winner 
-    if board[0] == board[1] == board[2] and board[1] != "-":
+    if board[0] == board[1] == board[2] and board[1] != " ":
         winner = board[0]
         return True
-    elif board[3] == board[4] == board[5] and board[4] != "-":
+    elif board[3] == board[4] == board[5] and board[4] != " ":
         winner = board[3]
         return True
-    elif board[6] == board[7] == board[8] and board[7] != "-":
+    elif board[6] == board[7] == board[8] and board[7] != " ":
         winner = board[6]
         return True
 
@@ -42,13 +42,13 @@ def checkDown(board):
     Checks for downward rows of X and makes a winner
     """
     global winner 
-    if board[0] == board[3] == board[6] and board[0] != "-":
+    if board[0] == board[3] == board[6] and board[0] != " ":
         winner = board[0]
         return True
-    elif board[1] == board[4] == board[7] and board[1] != "-":
+    elif board[1] == board[4] == board[7] and board[1] != " ":
         winner = board[1]
         return True
-    elif board[2] == board[5] == board[8] and board[2] != "-":
+    elif board[2] == board[5] == board[8] and board[2] != " ":
         winner = board[2]
         return True
 
@@ -57,10 +57,10 @@ def checkDiag(board):
     Checks for diagonal rows of X and makes a winner
     """
     global winner 
-    if board[0] == board[4] == board[8] and board[0] != "-":
+    if board[0] == board[4] == board[8] and board[0] != " ":
         winner = board[0]
         return True
-    elif board[6] == board[4] == board[2] and board[6] != "-":
+    elif board[6] == board[4] == board[2] and board[6] != " ":
         winner = board[6]
         return True
 
@@ -69,7 +69,7 @@ def checkDraw(board):
     Checks to see if the board is full while there is no winner, and then restarts the game
     """
     global gameRunning
-    if "-" not in board:
+    if " " not in board:
         print(boardGame)
         print("It's a draw, start again!")
         gameRunning = False
@@ -85,9 +85,9 @@ def restartGame():
     """
     global board, player1, winner, gameRunning
 
-    board = ["-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-"]
+    board = [" ", " ", " ",
+             " ", " ", " ",
+             " ", " ", " "]
     player1 = "X"
     winner = None
     gameRunning = True
@@ -106,7 +106,7 @@ def computer(board):
     """
     while player1 == 'O':
         position = random.randint(0,8)
-        if board[position] == "-":
+        if board[position] == " ":
             board[position] = "O"
             changePlayer()
 
